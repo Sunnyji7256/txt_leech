e2125d08dd03ea302ab649384c3b9202edf0083d
Sunnyji <151189921+Sunnyji7256@users.noreply.github.com>
2024-06-04 19:28:00 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -2,7 +2,7 @@ import os
 
 api_id = int(os.environ.get("API_ID", '21103068'))
 api_hash = os.environ.get("API_HASH", '633ab5bfab52d8763ffc6da7a9b2294a')
-bot_token = os.environ.get("BOT_TOKEN", '7292098277:AAHid6Gj-dcbHpx2-PYixZBX2EfEbUbvoMw')
+bot_token = os.environ.get("BOT_TOKEN", '7257484054:AAEq2_8TLILp12P-9aIwK0ZDuY-3HrEQWo0')
 
 
 OWNER = int(os.environ.get("OWNER", '6286919439'))
